01dbe1065b4147d059087ceae69a014b9ab6ee7a
Will <william.altxx@gmail.com>
2017-12-07 17:59:01 -0600

    syntax error in server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.JASWDB_URL) {
+if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
